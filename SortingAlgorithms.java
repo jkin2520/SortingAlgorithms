a60e836f7ad4c85eceb4086dd3b9c14740267246
skeez <117862122+jkin2520@users.noreply.github.com>
2024-11-29 09:44:20 -0500

    Update SortingAlgorithms.java
    updated code from gitignore

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,3 +1,4 @@
+package SortingAlgorithms.src.main;
 public class SortingAlgorithms {
 
     public static void quickSort(int[] array, int low, int high) {
@@ -24,4 +25,56 @@ public class SortingAlgorithms {
         array[high] = temp;
         return i + 1;
     }
-}
+    public static int[] mergeSort(int[] array) {
+        if (array.length <= 1) {
+            return array;
+        }
+
+        int mid = array.length / 2;
+        int[] left = mergeSort(java.util.Arrays.copyOfRange(array, 0, mid));
+        int[] right = mergeSort(java.util.Arrays.copyOfRange(array, mid, array.length));
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                merged[k++] = left[i++];
+            } else {
+                merged[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            merged[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            merged[k++] = right[j++];
+        }
+
+        return merged;
+    }
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+    }
+} 
+
+
